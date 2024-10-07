85dcea3a30af6a95de4f3df0a99d4b19aa39a02a
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-07 09:33:36 +0700

    Update useFetch, trying promise

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,39 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useState, useEffect, memo } from "react";
+import { memo } from "react";
+import useFetch from "../hooks/useFetch.js";
 import fetchAvailablePlaces from "../http.js";
 import Places from "./Places.jsx";
 import ErrorMessage from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 
 const AvailablePlaces = memo(({ onSelectPlace }) => {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetchData, setIsFetchData] = useState(false);
-  const [errorMsg, setErrorMsg] = useState();
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [isFetchData, setIsFetchData] = useState(false);
+  // const [errorMsg, setErrorMsg] = useState();
+  async function fetchSortingPlaces() {
+    const places = await fetchAvailablePlaces();
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(
+          places,
+          position.coords.latitude,
+          position.coords.longitude
+        );
+        resolve(sortedPlaces);
+      });
+    });
+  }
 
-  useEffect(() => {
-    const fetchDataPlaces = async () => {
-      setIsFetchData(true);
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetchData(false);
-        });
-      } catch (error) {
-        setErrorMsg({
-          message: error.message || "Failed to fetch places. Try Again Later!",
-        });
-        setIsFetchData(false);
-      }
-    };
-    fetchDataPlaces();
-  }, []);
+  const {
+    isLoading: isFetchData,
+    errorMsg,
+    placesData: availablePlaces,
+  } = useFetch(fetchSortingPlaces, []);
+
+  // useEffect(() => {
+  //   const fetchDataPlaces = async () => {
+  //     setIsFetchData(true);
+  //     try {
+  //       const places = await fetchAvailablePlaces();
+  //       navigator.geolocation.getCurrentPosition((position) => {
+  //         const sortedPlaces = sortPlacesByDistance(
+  //           places,
+  //           position.coords.latitude,
+  //           position.coords.longitude
+  //         );
+  //         setAvailablePlaces(sortedPlaces);
+  //         setIsFetchData(false);
+  //       });
+  //     } catch (error) {
+  //       setErrorMsg({
+  //         message: error.message || "Failed to fetch places. Try Again Later!",
+  //       });
+  //       setIsFetchData(false);
+  //     }
+  //   };
+  //   fetchDataPlaces();
+  // }, []);
 
   if (errorMsg) {
     return (
